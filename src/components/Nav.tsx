3bf221ef3eac4c2782532fb14b232aca1a84148b
Leigh Arriane Buendia <81524834+leigharriane@users.noreply.github.com>
2023-06-15 22:52:27 +0800

    nav

---
+++ src/components/Nav.tsx
@@ -0,0 +1,10 @@
+const Nav = () => {
+	return (
+		<div className='text-black fixed top-0 w-screen flex justify-between items-center px-[120px] py-[10px] font-sf'>
+			<p className='font-bold text-[#5B8CEC] text-[32px]'>LakbAI</p>
+			<button className='font-medium bg-[#5B8CEC] text-white rounded-full px-8 text-[20px] py-2'>Sign Up</button>
+		</div>
+	)
+}
+
+export default Nav;
