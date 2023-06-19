649d378805327bff93a285e93d7bce5ddca817ef
Leigh Arriane Buendia <81524834+leigharriane@users.noreply.github.com>
2023-06-19 23:46:06 +0800

    Update Nav.tsx

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1,6 +1,6 @@
 const Nav = () => {
 	return (
-		<div className='text-black fixed top-0 w-screen flex justify-between items-center px-[120px] py-[10px] font-sf'>
+		<div className='text-black fixed top-0 w-screen flex justify-between items-center px-[120px] py-[10px] font-sf bg-white'>
 			<p className='font-bold text-[#5B8CEC] text-[32px]'>LakbAI</p>
 			<button className='font-medium bg-[#5B8CEC] text-white rounded-full px-8 text-[20px] py-2'>Sign Up</button>
 		</div>
